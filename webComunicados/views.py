23df13dd6e7f1165b1e20070b4b3eb6c818655a7
Misaele <misaeltapiac71@gmail.com>
2023-10-24 00:14:12 -0300

    urls, views, templates

--- webComunicados/views.py
+++ webComunicados/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    title = "Comunicados"
+    if request.method == 'POST':
+        testing = request.POST.get('getlista')
+    else:
+        testing = 'nada'
+
+    data = {
+        "title": title,
+        "testing": testing,
+    }
+
+    return render(request,'webComunicados/index.html', data)
